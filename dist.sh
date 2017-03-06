843433a268150271cb9a7860af1951e8ea7cce4d
Stefan Dangl <s.dangl@hvmhv.de>
2017-03-06 07:52:32 +0100

    making things work on OSX

--- dist.sh
+++ dist.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-tstamp=$(stat -c%Y package.json)
+tstamp=$(stat -c%Y package.json || stat -f%m package.json)
 tstampo=$(cat .package.json.tstamp 2>/dev/null || true)
 if [ -z "$tstampo" ] || [ "$tstamp" -gt "$tstampo" ]; then
     echo "installing npm modules"
@@ -14,6 +14,11 @@ echo "compiling"
 ./gradlew build
 
 echo "browserifying"
+mkdir dist
+if ! [ -x $(which browserify) ]; then
+	echo "ERROR: You may need to install browserify via: npm install -g browserify"
+	exit 1
+fi
 browserify web/app.js -o dist/bundle.js
 cp web/kotlin.js dist/
 
